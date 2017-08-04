e613ecdc8263aba86afa76b487577d49385ff80e
Milton Câmara <miltomcamara@gmail.com>
2017-08-04 19:26:17 -0300

    Update app.module.ts
    Include CUSTOM_ELEMENTS_SCHEMA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -47,6 +47,7 @@ import { FooterComponent } from './components/shared/footer/footer.component';
     VeiculoModule,
     ReservaModule
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [AuthService, TransportadoraService, PeriodoService],
   bootstrap: [AppComponent]
 })
